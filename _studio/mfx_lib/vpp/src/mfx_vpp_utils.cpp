0b3a1a39ff955b798ad8cf02c548a743adc46d42
Jay Yang <jay.yang@intel.com>
2021-05-27 16:57:22 +0800

    [Kernel] Align kernel files to CS
    Issue: -
    Test: -

--- _studio/mfx_lib/vpp/src/mfx_vpp_utils.cpp
+++ _studio/mfx_lib/vpp/src/mfx_vpp_utils.cpp
@@ -77,9 +77,6 @@ const mfxU32 g_TABLE_DO_USE [] =
 #if (MFX_VERSION >= 1025)
     MFX_EXTBUFF_VPP_COLOR_CONVERSION,
 #endif
-#ifdef MFX_UNDOCUMENTED_VPP_VARIANCE_REPORT
-    MFX_EXTBUFF_VPP_VARIANCE_REPORT,
-#endif
     MFX_EXTBUFF_VPP_DEINTERLACING,
     MFX_EXTBUFF_VPP_VIDEO_SIGNAL_INFO,
     MFX_EXTBUFF_VPP_FIELD_PROCESSING,
@@ -637,14 +634,6 @@ void ShowPipeline( std::vector<mfxU32> pipelineList )
             }
 #endif
 
-#ifdef MFX_UNDOCUMENTED_VPP_VARIANCE_REPORT
-            case (mfxU32)MFX_EXTBUFF_VPP_VARIANCE_REPORT:
-            {
-                fprintf(stderr, "VARIANCE_REP \n");
-                break;
-            }
-#endif
-
             case (mfxU32)MFX_EXTBUFF_VPP_COMPOSITE:
             {
                 fprintf(stderr, "COMPOSITE \n");
@@ -823,15 +812,6 @@ void ReorderPipelineListForQuality( std::vector<mfxU32> & pipelineList )
         newList[index] = MFX_EXTBUFF_VPP_PROCAMP;
         index++;
     }
-
-#ifdef MFX_UNDOCUMENTED_VPP_VARIANCE_REPORT
-    /* [VarianceRep] FILTER */
-    if( IsFilterFound( &pipelineList[0], (mfxU32)pipelineList.size(), MFX_EXTBUFF_VPP_VARIANCE_REPORT ) )
-    {
-        newList[index] = MFX_EXTBUFF_VPP_VARIANCE_REPORT;
-        index++;
-    }
-#endif
 
     if( IsFilterFound( &pipelineList[0], (mfxU32)pipelineList.size(), MFX_EXTBUFF_VPP_FRAME_RATE_CONVERSION ) )
     {
@@ -2234,7 +2214,6 @@ void ConvertCaps2ListDoUse(MfxHwVideoProcessing::mfxVppCaps& caps, std::vector<m
     {
         list.push_back(MFX_EXTBUFF_VPP_IMAGE_STABILIZATION);
     }
-
 
     if(caps.uRotation)
     {
