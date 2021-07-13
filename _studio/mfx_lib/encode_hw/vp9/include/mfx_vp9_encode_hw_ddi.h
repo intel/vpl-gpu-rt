601c5b2eb42d0000f30511c99564dc01a0a4c267
Ilya Sergeyev <ilya.sergeev@intel.com>
2021-07-13 13:16:24 +0300

    [VP9e] Added MaxNumRef caps (#578) (#104)
    Need to merge after Driver change
    
    Issue: VSMGWL-35677
    Test: vp9_vdenc CI

--- _studio/mfx_lib/encode_hw/vp9/include/mfx_vp9_encode_hw_ddi.h
+++ _studio/mfx_lib/encode_hw/vp9/include/mfx_vp9_encode_hw_ddi.h
@@ -97,6 +97,7 @@ typedef struct tagENCODE_CAPS_VP9
     UINT            MaxPicHeight;
     USHORT          MaxNumOfDirtyRect;
     USHORT          MaxNumOfMoveRect;
+    UINT            MaxNum_Reference0;
 
 
 } ENCODE_CAPS_VP9;
