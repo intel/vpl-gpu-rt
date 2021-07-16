f49611c32da33f66e79817ecc264ae9aca08991f
Yuliya Bakhanova <yuliya.bakhanova@intel.com>
2021-07-16 12:29:16 +0300

    Inherit AdaptiveMaxFrameSize from init to reset (#126)

--- _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
+++ _studio/mfx_lib/shared/src/mfx_h264_enc_common_hw.cpp
@@ -5077,6 +5077,7 @@ void MfxHwH264Encode::InheritDefaultValues(
     InheritOption(parInit.mfx.BRCParamMultiplier, parReset.mfx.BRCParamMultiplier);
 
     InheritOption(extOpt3Init.ScenarioInfo, extOpt3Reset->ScenarioInfo);
+    InheritOption(extOpt3Init.AdaptiveMaxFrameSize, extOpt3Reset->AdaptiveMaxFrameSize);
     InheritOption(parInit.mfx.LowPower, parReset.mfx.LowPower);
     InheritOption(extOpt2Init.LookAheadDepth, extOpt2Reset->LookAheadDepth);
 
