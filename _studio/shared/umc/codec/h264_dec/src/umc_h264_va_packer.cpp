83fe2ea4cba465dd0e9620acf2920ff9e908adf6
luleilei1 <leileix.lu@intel.com>
2021-08-19 17:20:00 +0800

    Revert "enable center chroma sitting as default sample location (#302)" (#534)
    This reverts commit 567f86ec8ca54be8ac1aa4ab5561be24e08afe6e.

--- _studio/shared/umc/codec/h264_dec/src/umc_h264_va_packer.cpp
+++ _studio/shared/umc/codec/h264_dec/src/umc_h264_va_packer.cpp
@@ -704,7 +704,7 @@ void PackerVA::PackProcessingInfo(H264DecoderFrameInfo * sliceInfo)
     pipelineBuf->surface = m_va->GetSurfaceID(sliceInfo->m_pFrame->m_index); // should filled in packer
     pipelineBuf->additional_outputs = (VASurfaceID*)vpVA->GetCurrentOutputSurface();
     // To keep output aligned, decode downsampling use this fixed combination of chroma sitting type
-    pipelineBuf->input_color_properties.chroma_sample_location = VA_CHROMA_SITING_HORIZONTAL_CENTER | VA_CHROMA_SITING_VERTICAL_CENTER;
+    pipelineBuf->input_color_properties.chroma_sample_location = VA_CHROMA_SITING_HORIZONTAL_LEFT | VA_CHROMA_SITING_VERTICAL_CENTER;
 }
 #endif // #ifndef MFX_DEC_VIDEO_POSTPROCESS_DISABLE
 
