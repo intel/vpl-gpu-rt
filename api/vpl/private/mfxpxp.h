4e2cceb72ed20870728e2b2eaeecf10252fd314f
Ethan Wang <ethan.wang@intel.com>
2021-11-04 16:49:18 +0800

    [CP] Update PXP Header for secure decode (#1318)
    Remove useless structures
    Rename structures
    
    [Internal]
    Issue: N/A
    Test: manual
    
    Signed-off-by: Ethan Wang <ethan.wang@intel.com>

--- api/vpl/private/mfxpxp.h
+++ api/vpl/private/mfxpxp.h
@@ -37,41 +37,27 @@ File Name: mfxpxp.h
 extern "C" {
 #endif
 
-#if defined(ONEVPL_EXPERIMENTAL)
-#if (defined(_WIN32) || defined(_WIN64))
-#include <d3d11_4.h>
 MFX_PACK_BEGIN_STRUCT_W_PTR()
 /*! Describes secure decode configuration. */
 typedef struct {
-    /*! GUID needed for Decode ID3D11VideoContext3::CreateVideoDecoder*/
-    GUID* guidConfigBitstreamEncryption;
-    GUID* guidConfigMBcontrolEncryption;
-    GUID* guidConfigResidDiffEncryption;
-
-    /*! SnD/SnE needed for Decode ID3D11VideoContext3::DecoderBeginFrame1*/
-    D3D11_VIDEO_DECODER_BEGIN_FRAME_CRYPTO_SESSION *pBeginFrameCryptoSession;
-} *SecureDecodeConfigHDL, SecureDecodeConfig;
-MFX_PACK_END()
-#else
-MFX_PACK_BEGIN_STRUCT_W_PTR()
-/*! Describes secure decode configuration. */
-typedef struct {
-    mfxHDL                  pxpAttributesHdl;  /*!< Pointer to VAConfigAttrib. */
-    mfxU32                  ContextId;         /*!< context id of decoder. */
-} *SecureDecodeConfigHDL, SecureDecodeConfig;
+    mfxHDL                   pxpAttributesHdl;  /*!< Pointer to VAConfigAttrib. */
+    mfxU32                   ContextId;         /*!< context id of decoder. */
+    mfxU8                    reserved[56];      /*!< Reserved for future use. */
+} *mfxSecureDecodeConfigHDL, mfxSecureDecodeConfig;
 MFX_PACK_END()
 #endif
 
 MFX_PACK_BEGIN_STRUCT_W_PTR()
 /*! This is a table to map pxp parameter associacted with decode input.
-    PXP parameter pointer to VAEncryptionParameters on linux, and pointer to D3D11_VIDEO_DECODER_BUFFER_DESC2 on windows.
+    PXP parameter point to VAEncryptionParameters on linux.
     pMfxBitstream is the key to search pPXPParams
     {pMfxBitstream | pPXPParams}
 */
 typedef struct {
-    mfxBitstream            *pMfxBitstream;   /*!< Pointer to input bitstream buffer. */
-    mfxHDL                   pPXPParams;      /*!< Pointer to PXP parameters. */
-} *DecodeParamMapHDL, DecodeParamMap;
+    mfxBitstream            *pMfxBitstream;     /*!< Pointer to input bitstream buffer. */
+    mfxHDL                   pPXPParams;        /*!< Pointer to PXP parameters. */
+    mfxU8                    reserved[24];      /*!< Reserved for future use. */
+} *mfxDecodeParamMapHDL, mfxDecodeParamMap;
 MFX_PACK_END()
 
 MFX_PACK_BEGIN_STRUCT_W_PTR()
@@ -91,18 +77,18 @@ MFX_PACK_BEGIN_STRUCT_W_PTR()
         mfxHDL     pxpSessionHdl;
         VAConfigID config_id;
         vaStatus = vaCreatedProtectedSession(va_dpy, config_id, &pxpSessionHdl);
-    3. Create PXPCtx and set handle to VPL
-        PXPCtx pxp_ctx_hdl;
+    3. Create mfxPXPCtx and set handle to VPL
+        mfxPXPCtx pxp_ctx_hdl;
         pxp_ctx_hdl.pxpSessionHdl = pxpSessionHdl;
         MfxStatus = MFXCORE_SetHandle(mfx_session, MFX_HANDLE_CP_CONTEXT, (mfxHDL)&pxp_ctx_hdl);
-    4. Set structure SecureDecodeConfig and Initialize decoder
-        //set pxp_ctx_hdl.secureDecodeCfg reference describe of SecureDecodeConfig
+    4. Set structure mfxSecureDecodeConfig and Initialize decoder
+        //set pxp_ctx_hdl.secureDecodeCfg reference describe of mfxSecureDecodeConfig
         mfxStatus = MFXVideoDECODE_Init(mfx_session, &init_param);
-    5. Set structure SecureEncodeConfig and Initialize encoder
-        //set pxp_ctx_hdl.secureEncodeCfg reference describe of SecureEncodeConfig
+    5. Set structure mfxSecureEncodeConfig and Initialize encoder
+        //set pxp_ctx_hdl.secureEncodeCfg reference describe of mfxSecureEncodeConfig
         mfxStatus = MFXVideoENCODE_Init(mfx_session, &init_param);
-    7. Set structure SecureVPPConfig and Initialize VPP
-        //set pxp_ctx_hdl.secureVPPCfg reference describe of SecureVPPConfig
+    7. Set structure mfxSecureVPPConfig and Initialize VPP
+        //set pxp_ctx_hdl.secureVPPCfg reference describe of mfxSecureVPPConfig
         mfxStatus = MFXVideoVPP_Init(mfx_session, &init_param);
     8. Attach PXP session and context
         vaStatus  = vaAttachProtectedSession(va_dpy, pxpSessionHdl, pxp_ctx_hdl.secureDecodeCfg.ContextId);
@@ -114,13 +100,13 @@ MFX_PACK_BEGIN_STRUCT_W_PTR()
         mfxFrameSurface1 *surface_out_0;
         mfxHDL pPXPParams_0;
         mfxSyncPoint syncpD;
-        //set pxp_ctx_hdl.decodeParamMapHdl reference describe of DecodeParamMapHDL
+        //set pxp_ctx_hdl.decodeParamMapHdl reference describe of mfxDecodeParamMapHDL
         //pxp_ctx_hdl.decodeParamMapHdl = {&bs_0, &surface_out_0, &pPXPParams_0}
         mfxStatus = MFXVideoDECODE_DecodeFrameAsync(mfx_session, &bs_0, surface_work, &surface_out_0, &syncpD);
         mfxBitstrem bs_1;
         mfxFrameSurface1 *surface_out_1;
         mfxHDL pPXPParams_1;
-        //set pxp_ctx_hdl.decodeParamMapHdl reference describe of DecodeParamMapHDL
+        //set pxp_ctx_hdl.decodeParamMapHdl reference describe of mfxDecodeParamMapHDL
         //pxp_ctx_hdl.decodeParamMapHdl = {&bs_0, &surface_out_0, &pPXPParams_0}, {&bs_1, &surface_out_1, &pPXPParams_1}
         mfxStatus = MFXVideoDECODE_DecodeFrameAsync(mfx_session, &bs_1, surface_work, &surface_out_1, &syncpD);
         MFXVideoCORE_SyncOperation(mfx_session, syncpD, INFINITE);
@@ -152,17 +138,16 @@ MFX_PACK_BEGIN_STRUCT_W_PTR()
         MFXUnload(mfx_loader);
 */
 typedef struct {
-    mfxHDL                  pxpSessionHdl;     /*!< The handle of PXP Session.                     */
-    SecureDecodeConfig      secureDecodeCfg;   /*!< Pointer to SecureDecodeConfig structure.       */
+    mfxHDL                   pxpSessionHdl;     /*!< The handle of PXP Session. */
+    mfxSecureDecodeConfig    secureDecodeCfg;   /*!< Pointer to mfxSecureDecodeConfig structure. */
 
-    DecodeParamMapHDL       decodeParamMapHdl; /*!< Pointer to DecodeParamMapHDL structure.        */
-    mfxU32                  decodeParamMapCnt; /*!< The count of PXP parameters.                   */
-} PXPCtx, *PXPCtxHDL;
+    mfxDecodeParamMapHDL     decodeParamMapHdl; /*!< Pointer to mfxDecodeParamMapHDL structure. */
+    mfxU32                   decodeParamMapCnt; /*!< The count of PXP parameters. */
+} mfxPXPCtx, *mfxPXPCtxHDL;
 MFX_PACK_END()
 
 #ifdef __cplusplus
 } // extern "C"
 #endif
 
-#endif // ONEVPL_EXPERIMENTAL
 #endif //__MFX_PXP_H__
