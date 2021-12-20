8ad5e51e66a68b155681754260ee6c7da795e5ea
gfxVPLsdm <gfxvplsdm@intel.com>
2021-12-20 23:17:39 +0800

    Revert "CM: refactored CM integration " (#1804)
    This reverts commit 3ed40aeffb2bc8a66c470581932eb0f87885a339
    
    Issue: MDP-68607
    Test: no
    
    Co-authored-by: Dmitry Ermilov <dmitry.ermilov@intel.com>

--- _studio/mfx_lib/shared/src/mfx_session.cpp
+++ _studio/mfx_lib/shared/src/mfx_session.cpp
@@ -457,12 +457,19 @@ mfxStatus _mfxVersionedSessionImpl::InitEx(mfxInitParam& par)
         return MFX_ERR_UNSUPPORTED;
     }
 
-    if (par.GPUCopy != MFX_GPUCOPY_DEFAULT)
+    // Windows: By default CM Copy enabled on HW cores, so only need to handle explicit OFF value
+    // Linux: By default CM Copy disabled on HW cores so only need to handle explicit ON value
+    //        Also see the logic in SetHandle from VAAPI core
+    const bool disableGpuCopy = (m_pCORE->GetVAType() == MFX_HW_VAAPI )
+        ? (MFX_GPUCOPY_ON != par.GPUCopy)
+        : (MFX_GPUCOPY_OFF == par.GPUCopy);
+
+    if (disableGpuCopy)
     {
         CMEnabledCoreInterface* pCmCore = QueryCoreInterface<CMEnabledCoreInterface>(m_pCORE.get());
         if (pCmCore)
         {
-            pCmCore->SetCmCopyStatus(MFX_GPUCOPY_ON == par.GPUCopy);
+            pCmCore->SetCmCopyStatus(false);
         }
     }
 
