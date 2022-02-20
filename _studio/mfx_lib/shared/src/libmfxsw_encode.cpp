b084f7ad2410781c4c0cb4a20dbbb8dc7140f328
gfxVPLsdm <gfxvplsdm@intel.com>
2022-02-20 14:26:10 +0800

    [AV1 HWA] Switch CodecID (#2181)
    Co-authored-by: Neelesh Gokhale <neelesh.gokhale@intel.com>

--- _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
+++ _studio/mfx_lib/shared/src/libmfxsw_encode.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2008-2021 Intel Corporation
+// Copyright (c) 2008-2022 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -311,7 +311,7 @@ static const CodecId2Handlers codecId2Handlers =
             {
             }
          }
-    }
+    },
 #endif // MFX_ENABLE_AV1_VIDEO_ENCODE
 }; // codecId2Handlers
 
