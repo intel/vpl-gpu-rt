e96e72e21e0bebfdde8181bfe32a38c5cdbd7f79
gfxVPLsdm <gfxvplsdm@intel.com>
2022-03-07 16:44:09 +0800

    [Decode] check the value of array index (#1946)
    Co-authored-by: chuanli1 <chuan1.liu@intel.com>

--- _studio/shared/umc/codec/jpeg_dec/src/umc_mjpeg_mfx_decode_hw.cpp
+++ _studio/shared/umc/codec/jpeg_dec/src/umc_mjpeg_mfx_decode_hw.cpp
@@ -445,6 +445,8 @@ Status MJPEGVideoDecoderMFX_HW::GetFrameHW(MediaDataEx* in)
                 return UMC_ERR_FAILED;
 
             jerr = m_decBase->ParseSOS(JO_READ_HEADER);
+            if (m_decBase->m_curr_comp_no != m_decBase->m_curr_scan->ncomps-1)
+                return UMC_ERR_INVALID_STREAM;
             if(JPEG_OK != jerr)
             {
                 if (JPEG_ERR_BUFF == jerr)
