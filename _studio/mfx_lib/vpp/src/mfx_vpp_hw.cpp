d1337acd0e35c029a1a5e7217b84b703d55709b8
gfxVPLsdm <gfxvplsdm@intel.com>
2022-03-23 15:32:32 +0800

    Revert "[VPP] Switch to VPP_SYNC_WORKLOAD when input is video memory " (#2498)
    This reverts commit 812e8926bedf539b007c143e8d72970300f886ed
    
    Co-authored-by: luleilei1 <leileix.lu@intel.com>

--- _studio/mfx_lib/vpp/src/mfx_vpp_hw.cpp
+++ _studio/mfx_lib/vpp/src/mfx_vpp_hw.cpp
@@ -2455,12 +2455,8 @@ mfxStatus  VideoVPPHW::Init(
         m_config.m_surfCount[VPP_IN] = request.NumFrameMin;
     }
 
-    // VA usage need to fully utilize VDBox engine. In ASYNC mode, there is a high cost synchronization, 
-    // i.e., it usually takes 1~2ms, which breaks CPU/GPU concurrency - CPU has to be blocked there and waiting for HW completion, 
-    // so that cannot proceed to prepare GPU workloads for next frames, therefore cannot make GPU engines fully busy.
-    // Enable VPP_SYNC_WORKLOAD by default when app operates with input video memory. 
-    // For input system memory cases use VPP_ASYNC_WORKLOAD mode.
-    m_workloadMode = (D3D_TO_D3D == m_ioMode || D3D_TO_SYS == m_ioMode) ? VPP_SYNC_WORKLOAD : VPP_ASYNC_WORKLOAD;
+    // async workload mode by default
+    m_workloadMode = VPP_ASYNC_WORKLOAD;
 
     //-----------------------------------------------------
     // [4] resource and task manager
@@ -2986,12 +2982,8 @@ mfxStatus VideoVPPHW::Reset(mfxVideoParam *par)
         m_config.m_surfCount[VPP_IN] = request.NumFrameMin;
     }
 
-    // VA usage need to fully utilize VDBox engine. In ASYNC mode, there is a high cost synchronization, 
-    // i.e., it usually takes 1~2ms, which breaks CPU/GPU concurrency - CPU has to be blocked there and waiting for HW completion, 
-    // so that cannot proceed to prepare GPU workloads for next frames, therefore cannot make GPU engines fully busy.
-    // Enable VPP_SYNC_WORKLOAD by default when app operates with input video memory. 
-    // For input system memory cases use VPP_ASYNC_WORKLOAD mode.
-    m_workloadMode = (D3D_TO_D3D == m_ioMode || D3D_TO_SYS == m_ioMode) ? VPP_SYNC_WORKLOAD : VPP_ASYNC_WORKLOAD;
+    // async workload mode by default
+    m_workloadMode = VPP_ASYNC_WORKLOAD;
 
     //-----------------------------------------------------
     // [5] resource and task manager
