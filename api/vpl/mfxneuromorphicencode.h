546a3e628f54312192bef92a58a3f62d595dbc5e
Miroslav Goncharenko <miroslav.goncharenko@intel.com>
2022-08-19 16:53:50 +0200

    api: Neuromorphic encode (#3378)

---
+++ api/vpl/mfxneuromorphicencode.h
@@ -0,0 +1,21 @@
+/*############################################################################
+  # Copyright Intel Corporation
+  #
+  # SPDX-License-Identifier: MIT
+  ############################################################################*/
+
+#ifndef __MFXNEUROMORPHICENCODE_H__
+#define __MFXNEUROMORPHICENCODE_H__
+
+#include "mfxdefs.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+#endif
+
