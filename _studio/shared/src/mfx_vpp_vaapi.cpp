edcfb6670f75c8772b6c16c80ed73cd1cb33db60
Haihao Xiang <haihao.xiang@intel.com>
2022-11-09 13:46:27 +0800

    [VPP] Don't add multiple buffers of VAProcFilterParameterBufferHDRToneMapping
    Signed-off-by: Haihao Xiang <haihao.xiang@intel.com>

--- _studio/shared/src/mfx_vpp_vaapi.cpp
+++ _studio/shared/src/mfx_vpp_vaapi.cpp
@@ -1565,6 +1565,12 @@ mfxStatus VAAPIVideoProcessing::Execute(mfxExecuteParams *pParams)
     VAHdrMetaDataHDR10 inHDR10MetaData = {};
     if(pParams->inHDR10MetaData.enabled)
     {
+        if (m_hdrtmFilterID != VA_INVALID_ID)
+        {
+            mfxSts = RemoveBufferFromPipe(m_hdrtmFilterID);
+            MFX_CHECK_STS(mfxSts);
+        }
+
         inHDR10MetaData = SetHdrMetaData(pParams->inHDR10MetaData);
         VAProcFilterParameterBufferHDRToneMapping hdrtm_param = {};
         hdrtm_param.type                                      = VAProcFilterHighDynamicRangeToneMapping;
