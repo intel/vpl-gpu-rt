8756599ca0f7db3e6c3c2af8476d9bb8cc0a9371
gfxVPLsdm <gfxvplsdm@intel.com>
2023-01-06 12:13:34 +0800

    revise according to review (#4297)
    Co-authored-by: Yao, Leyu <leyu.yao@intel.com>

--- _studio/mfx_lib/encode_hw/av1/agnostic/base/av1ehw_base_general.cpp
+++ _studio/mfx_lib/encode_hw/av1/agnostic/base/av1ehw_base_general.cpp
@@ -3588,6 +3588,11 @@ mfxStatus General::CopyConfigurable(const ParamSupport& sprt, const mfxVideoPara
             CopyEB(copyIt->second, pEbIn, pEbTmp);
         }
 
+        if(pEbOut->BufferId == MFX_EXTBUFF_AV1_AUXDATA)
+        {
+            std::copy_n((mfxU8*)pEbIn, pEbIn->BufferSz, ebTmp.data());
+        }
+
         std::copy_n(ebTmp.data(), ebTmp.size(), (mfxU8*)pEbOut);
     });
 
