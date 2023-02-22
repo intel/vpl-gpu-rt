0e1470a30fce58a3211bb232b781fe3f08552543
gfxVPLsdm <gfxvplsdm@intel.com>
2023-02-22 22:40:29 +0800

    [Encode] Fix vp9e multiref/LF delta for MTL+ (#4596)
    Multi-ref and LF delta changes for MTL+ platforms
    
    Co-authored-by: Shiryaeva, Ekaterina <ekaterina.shiryaeva.alexandrovna@intel.com>

--- _studio/mfx_lib/shared/include/mfx_platform_caps.h
+++ _studio/mfx_lib/shared/include/mfx_platform_caps.h
@@ -105,6 +105,16 @@ namespace VP9ECaps {
     {
         return (platform >= MFX_HW_DG2);
     }
+
+    inline bool IsTableBasedLfLevelUsed(eMFXHWType platform) 
+    {
+        return (platform >= MFX_HW_MTL);
+    }
+
+    inline bool IsDefaultMultiRefUsed(eMFXHWType platform) 
+    {
+        return (platform >= MFX_HW_MTL);
+    }
 }
 #endif
 
