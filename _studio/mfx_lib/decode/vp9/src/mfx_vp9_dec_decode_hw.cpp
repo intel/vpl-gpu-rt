059775b9e812526625043322cf774e4341e334da
gfxVPLsdm <gfxvplsdm@intel.com>
2023-11-20 10:13:22 +0800

    [Media Common][VP] Fix STL deque thread safety issue (#6097)
    Fix thread safety issue
    
    Co-authored-by: Gu, Lihao <lihao.gu@intel.com>

--- _studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode_hw.cpp
+++ _studio/mfx_lib/decode/vp9/src/mfx_vp9_dec_decode_hw.cpp
@@ -941,12 +941,13 @@ mfxStatus MFX_CDECL VP9DECODERoutine(void *p_state, void * /* pp_param */, mfxU3
     if (data.showFrame)
     {
         MFX_CHECK(data.surface_work, MFX_ERR_UNDEFINED_BEHAVIOR);
-
-        if (data.currFrameId >= (mfxI32)decoder.m_mCopyGuard.size())
         {
-            decoder.m_mCopyGuard.resize(data.currFrameId + 1);
+            UMC::AutomaticUMCMutex guard(decoder.m_mGuard);
+            if (data.currFrameId >= (mfxI32)decoder.m_mCopyGuard.size())
+            {
+                decoder.m_mCopyGuard.resize(data.currFrameId + 1);
+            }
         }
-
         UMC::AutomaticUMCMutex guardCopy(decoder.m_mCopyGuard[data.currFrameId]);
         mfxStatus sts = decoder.m_surface_source->PrepareToOutput(data.surface_work, data.currFrameId, 0, decoder.m_core->GetVAType() == MFX_HW_VAAPI);
         MFX_CHECK_STS(sts);
