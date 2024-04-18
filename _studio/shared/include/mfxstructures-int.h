4170dd9fa1ea319dda81b6189616ecc9b178a321
gfxVPLsdm <gfxvplsdm@intel.com>
2024-04-19 02:05:47 +0800

    [RT Common] DevID for Arc A750E and A580E export fused SKUs secured (#6584)
    Co-authored-by: Yuan, Jenny <jenny.yuan@intel.com>

--- _studio/shared/include/mfxstructures-int.h
+++ _studio/shared/include/mfxstructures-int.h
@@ -538,6 +538,8 @@ typedef struct {
     { 0x56BB, MFX_HW_DG2, MFX_GT4 }, // DG2
     { 0x56BC, MFX_HW_DG2, MFX_GT4 }, // DG2
     { 0x56BD, MFX_HW_DG2, MFX_GT4 }, // DG2
+    { 0x56BE, MFX_HW_DG2, MFX_GT4 }, // DG2
+    { 0x56BF, MFX_HW_DG2, MFX_GT4 }, // DG2
     { 0x56C2, MFX_HW_DG2, MFX_GT4 }, // DG2
 
     /* PVC */
