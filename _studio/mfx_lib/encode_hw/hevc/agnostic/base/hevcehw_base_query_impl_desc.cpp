98d7c90b504f3ccfc0f31353567a3fd10f853ca2
Yuan, Jenny <jenny.yuan@intel.com>
2025-04-11 19:38:10 +0000

    [HEVCe] Export more information in QueryImplsDescription
    Extend QueryImplDesc::QueryImplsDescription to provide more details about encoding capabilities:
    List of supported extBuffers; List of supported BRCs; Target Bit depth and target chroma
    subsamplings supported by HEVC encoder.

--- _studio/mfx_lib/encode_hw/hevc/agnostic/base/hevcehw_base_query_impl_desc.cpp
+++ _studio/mfx_lib/encode_hw/hevc/agnostic/base/hevcehw_base_query_impl_desc.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Intel Corporation
+// Copyright (c) 2020-2025 Intel Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -22,6 +22,7 @@
 #if defined(MFX_ENABLE_H265_VIDEO_ENCODE)
 
 #include "hevcehw_base_query_impl_desc.h"
+#include "hevcehw_base_legacy.h"
 
 using namespace HEVCEHW;
 using namespace HEVCEHW::Base;
@@ -61,15 +62,49 @@ void QueryImplDesc::QueryImplsDescription(const FeatureBlocks& blocks, TPushQID 
             , MFX_FOURCC_IYUV
             , MFX_FOURCC_I010
         };
+#ifdef ONEVPL_EXPERIMENTAL
+        const mfxU16 ChromaFormat[] =
+        {
+            MFX_CHROMAFORMAT_YUV420
+            , MFX_CHROMAFORMAT_YUV422
+            , MFX_CHROMAFORMAT_YUV444
+        };
+        const mfxU16 RateControl[] =
+        {
+            MFX_RATECONTROL_CBR
+            , MFX_RATECONTROL_VBR
+            , MFX_RATECONTROL_CQP
+            , MFX_RATECONTROL_ICQ
+            , MFX_RATECONTROL_VCM
+            , MFX_RATECONTROL_QVBR
+        };
+        const mfxU32 ExtendedBuffer[] =
+        {
+            MFX_EXTBUFF_CODING_OPTION
+            , MFX_EXTBUFF_CODING_OPTION2
+            , MFX_EXTBUFF_CODING_OPTION3
+            , MFX_EXTBUFF_HEVC_TILES
+            , MFX_EXTBUFF_HEVC_PARAM
+            , MFX_EXTBUFF_HEVC_REGION
+            , MFX_EXTBUFF_HEVC_REFLIST_CTRL
+            , MFX_EXTBUFF_HEVC_REFLISTS
+            , MFX_EXTBUFF_HEVC_TEMPORAL_LAYERS
+            , MFX_EXTBUFF_ENCODER_ROI
+            , MFX_EXTBUFF_MBQP
+            , MFX_EXTBUFF_DIRTY_RECTANGLES
+            , MFX_EXTBUFF_PRED_WEIGHT_TABLE
+        };
+#endif
         struct Config
         {
             mfxU16 Profile;
             mfxU32 FourCC;
             mfxU16 BitDepth;
             mfxU16 Shift;
-            mfxU16 CromaFormat;
+            mfxU16 ChromaFormat;
             mfxU16 LowPower;
         };
+
         const Config cfgs[] =
         {
               {MFX_PROFILE_HEVC_MAIN  , MFX_FOURCC_NV12,  8, 0, MFX_CHROMAFORMAT_YUV420, MFX_CODINGOPTION_UNKNOWN}
@@ -95,6 +130,10 @@ void QueryImplDesc::QueryImplsDescription(const FeatureBlocks& blocks, TPushQID 
         auto& queryNC = FeatureBlocks::BQ<FeatureBlocks::BQ_Query1NoCaps>::Get(blocks);
         auto& queryWC = FeatureBlocks::BQ<FeatureBlocks::BQ_Query1WithCaps>::Get(blocks);
 
+#ifdef ONEVPL_EXPERIMENTAL
+        const auto& blockCheckBRC = FeatureBlocks::Get(queryWC, { FEATURE_LEGACY, Legacy::BLK_CheckBRC });
+#endif
+
         caps.CodecID                 = MFX_CODEC_HEVC;
         caps.MaxcodecLevel           = (MFX_TIER_HEVC_HIGH | MFX_LEVEL_HEVC_62);
         caps.BiDirectionalPrediction = 0;
@@ -104,8 +143,16 @@ void QueryImplDesc::QueryImplsDescription(const FeatureBlocks& blocks, TPushQID 
             mfxRange32U Width  = {0xffffffff, 0, 0xffffffff};
             mfxRange32U Height = {0xffffffff, 0, 0xffffffff};
             std::list<mfxU32> FourCC;
+#ifdef ONEVPL_EXPERIMENTAL
+            std::list<mfxU16> TargetChromaSubsamplings;
+            mfxU16 TargetMaxBitDepth = 0;
+#endif
         };
         std::map<mfxU32, TmpCaps> tmpCaps;
+
+#ifdef ONEVPL_EXPERIMENTAL
+        std::list<mfxU16> supportedRC;
+#endif
 
         for (auto& cfg : cfgs)
         {
@@ -119,22 +166,44 @@ void QueryImplDesc::QueryImplsDescription(const FeatureBlocks& blocks, TPushQID 
             in.mfx.FrameInfo.Width           = 1920;
             in.mfx.FrameInfo.Height          = 1088;
             in.mfx.FrameInfo.FourCC          = cfg.FourCC;
-            in.mfx.FrameInfo.ChromaFormat    = cfg.CromaFormat;
+            in.mfx.FrameInfo.ChromaFormat    = cfg.ChromaFormat;
             in.mfx.FrameInfo.BitDepthLuma    = cfg.BitDepth;
             in.mfx.FrameInfo.Shift           = cfg.Shift;
             in.mfx.LowPower                  = cfg.LowPower;
 
             auto pCO3 = (mfxExtCodingOption3*)in.NewEB(MFX_EXTBUFF_CODING_OPTION3);
-            out.NewEB(MFX_EXTBUFF_CODING_OPTION3);
+            auto pCO3_out = (mfxExtCodingOption3*)out.NewEB(MFX_EXTBUFF_CODING_OPTION3);
 
             MFX_CHECK(pCO3, MFX_ERR_UNKNOWN);
             pCO3->TargetBitDepthLuma      = cfg.BitDepth;
-            pCO3->TargetChromaFormatPlus1 = cfg.CromaFormat + 1;
+            pCO3->TargetChromaFormatPlus1 = cfg.ChromaFormat + 1;
 
             if (MFX_ERR_NONE != RunBlocks(CheckGE<mfxStatus, MFX_ERR_NONE>, queryNC, in, out, tmpStorage))
                 continue;
+
+#ifdef ONEVPL_EXPERIMENTAL
+            if (out.mfx.LowPower != MFX_CODINGOPTION_ON)
+                continue;
+#endif
+
             if (MFX_ERR_NONE != RunBlocks(CheckGE<mfxStatus, MFX_ERR_NONE>, queryWC, in, out, tmpStorage))
                 continue;
+
+#ifdef ONEVPL_EXPERIMENTAL
+            auto IsSupportedRateControl = [&](mfxU16 rc)
+            {
+                out.mfx.RateControlMethod = rc;
+                return blockCheckBRC->Call(in, out, tmpStorage) == MFX_ERR_NONE;
+            };
+
+            for (mfxU16 rc : RateControl)
+            {
+                if (std::find(supportedRC.begin(), supportedRC.end(), rc) != supportedRC.end())
+                    continue;
+                if (IsSupportedRateControl(rc))
+                    supportedRC.push_back(rc);
+            }
+#endif
 
             auto& currCaps = tmpCaps[cfg.Profile];
             Defaults::Param defs(
@@ -142,12 +211,30 @@ void QueryImplDesc::QueryImplsDescription(const FeatureBlocks& blocks, TPushQID 
                 , Glob::EncodeCaps::Get(tmpStorage)
                 , core.GetHWType()
                 , Glob::Defaults::Get(tmpStorage));
+
+            mfxU32 step = defs.base.GetCodedPicAlignment(defs);
+
+#ifdef ONEVPL_EXPERIMENTAL
+            auto IsUnsupportedChromaFormat = [&](mfxU16 chroma_format)
+            {
+                pCO3_out->TargetChromaFormatPlus1 = chroma_format + 1;
+                return defs.base.CheckTargetChromaFormat(defs, out) != MFX_ERR_NONE;
+            };
+
+            std::list<mfxU16> supportedChromaFormat(std::begin(ChromaFormat), std::end(ChromaFormat));
+            supportedChromaFormat.remove_if(IsUnsupportedChromaFormat);
+            currCaps.TargetChromaSubsamplings.splice(currCaps.TargetChromaSubsamplings.end(), supportedChromaFormat);
+
+            mfxU16 bitDepth = defs.base.GetMaxBitDepth(defs);
+
+            CheckMinOrClip(currCaps.TargetMaxBitDepth, bitDepth);
+#endif
+
             auto IsUnsupportedFourCC = [&](mfxU32 fcc)
             {
                 out.mfx.FrameInfo.FourCC = fcc;
                 return defs.base.CheckFourCC(defs, out) != MFX_ERR_NONE;
             };
-            mfxU32 step = defs.base.GetCodedPicAlignment(defs);
 
             std::list<mfxU32> supportedFourCC(std::begin(FourCC), std::end(FourCC));
             supportedFourCC.remove_if(IsUnsupportedFourCC);
@@ -184,6 +271,22 @@ void QueryImplDesc::QueryImplsDescription(const FeatureBlocks& blocks, TPushQID 
                     ah.PushBack(memCaps.ColorFormats) = fcc;
                     ++memCaps.NumColorFormats;
                 }
+
+#ifdef ONEVPL_EXPERIMENTAL
+                intCaps.second.TargetChromaSubsamplings.sort();
+                intCaps.second.TargetChromaSubsamplings.unique();
+
+                auto& memCapsExt = ah.PushBack(memCaps.MemExtDesc);
+                memCapsExt.Version.Version = MFX_STRUCT_VERSION(1, 0);
+
+                for (mfxU16 chroma_format : intCaps.second.TargetChromaSubsamplings)
+                {
+                    ah.PushBack(memCapsExt.TargetChromaSubsamplings) = chroma_format;
+                    ++memCapsExt.NumTargetChromaSubsamplings;
+                }
+
+                memCapsExt.TargetMaxBitDepth = intCaps.second.TargetMaxBitDepth;
+#endif
             }
 
             ah.PushBack(profileCaps.MemDesc);
@@ -195,6 +298,25 @@ void QueryImplDesc::QueryImplsDescription(const FeatureBlocks& blocks, TPushQID 
 
             ++caps.NumProfiles;
         }
+
+#ifdef ONEVPL_EXPERIMENTAL
+        auto& extDesc = ah.PushBack(caps.EncExtDesc);
+        extDesc.Version.Version = MFX_STRUCT_VERSION(1, 0);
+
+        supportedRC.sort();
+
+        for (mfxU16 rc : supportedRC)
+        {
+            ah.PushBack(extDesc.RateControlMethods) = rc;
+            ++extDesc.NumRateControlMethods;
+        }
+
+        for (mfxU32 ext_buf_id : ExtendedBuffer)
+        {
+            ah.PushBack(extDesc.ExtBufferIDs) = ext_buf_id;
+            ++extDesc.NumExtBufferIDs;
+        }
+#endif
 
         return MFX_ERR_NONE;
     };
