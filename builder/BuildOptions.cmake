e52d5ddad2d506ef2b2fa36ce88cdbb91663aa78
Yuan, Jenny <jenny.yuan@intel.com>
2025-04-24 01:19:44 +0000

    [RT Common] Refine revenue build
    Refine revenue build:
    1. Supports open source in BuildOptionsPlatform.cmake
    2. Directly use platform name as platform macro name in makefiles.
       Supports using platform macro in makefiles for both close source
       and open source.

--- builder/BuildOptions.cmake
+++ builder/BuildOptions.cmake
@@ -79,6 +79,8 @@ if (BUILD_KERNELS)
 endif()
 
 
+include(${BUILDER_ROOT}/BuildOptionsPlatform.cmake)
+
 option( MFX_ENABLE_VVC_VIDEO_DECODE "Enabled VVC decoder?" ON)
 option( MFX_ENABLE_AV1_VIDEO_DECODE "Enabled AV1 decoder?" ON)
 option( MFX_ENABLE_VP8_VIDEO_DECODE "Enabled VP8 decoder?" ON)
