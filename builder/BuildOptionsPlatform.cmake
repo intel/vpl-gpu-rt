3a2ec9d00169fde835cabf6dcdac4bcdf91f25bf
Wang, Apple <apple.wang@intel.com>
2025-12-31 01:40:58 +0000

    [Media Common] Add NVL platform support to build configuration
    Added 'NVL' to the list of supported graphics platforms in the build system configuration.

--- builder/BuildOptionsPlatform.cmake
+++ builder/BuildOptionsPlatform.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Intel Corporation
+# Copyright (c) 2024-2025 Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -24,7 +24,7 @@ message( STATUS "Configuration of Supported Platforms" )
   # Supported Platforms
   set(BS_GFX_PLATFORMS_SUPPORTED
       "TGL" "RKL" "DG1" "ADL" "RPL" "DG2" "MTL" "ARL"
-      "LNL" "BMG" "PTL"
+      "LNL" "BMG" "PTL" "NVL"
   )
 
 message( STATUS "Supported platforms = ${BS_GFX_PLATFORMS_SUPPORTED}" )
